fde3b75247c02891956c8247610d6108875164fa
Akmal Soliev <soliev@gmail.com>
2022-11-10 15:13:55 +0100

    Removed github authenticator, not required

--- DeepLearningDevelopment/setup.sh
+++ DeepLearningDevelopment/setup.sh
@@ -30,17 +30,6 @@ wget https://github.com/Peltoche/lsd/releases/download/${latestVersion}/${fileNa
 rm $fileName
 
 ###################################################################################################
-# Installing Github Authenticator
-type -p curl >/dev/null ||  apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |  dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-  &&  chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-  &&  apt update \
-  &&  apt install gh -y
-apt-get update
-apt install gh
-
-###################################################################################################
 # Copying the configuration files to USER_DIR dir
 cp -r "${SOURCE_DIR}"/config/. $USER_DIR
 # Vim setting and installing plugins
