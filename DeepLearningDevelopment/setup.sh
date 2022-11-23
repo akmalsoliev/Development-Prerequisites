07eaea619d629e1620c49f98d98f0055232b70da
Akmal Soliev <soliev@gmail.com>
2022-11-23 11:21:32 +0100

    Conquer of Completion added

--- DeepLearningDevelopment/setup.sh
+++ DeepLearningDevelopment/setup.sh
@@ -32,13 +32,15 @@ rm $fileName
 ###################################################################################################
 # Copying the configuration files to USER_DIR dir
 cp -r "${SOURCE_DIR}"/config/. $USER_DIR
+
 # Vim setting and installing plugins
-# 1.
-# Plugin Manager
+
+# 1. Plugin Manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'source ~/.vimrc' +'PlugInstall!' +'PlugUpdate!' +qa
-# 2.
-# Setting Auto Completion Station
+echo "colorscheme nord" >> ~/.vimrc
+
+# 2. Setting Auto Completion Station
 curl -fsSL https://deb.nodesource.com/setup_19.x | bash - && \
   apt-get install -y nodejs
 corepack enable && corepack prepare yarn@stable --activate
